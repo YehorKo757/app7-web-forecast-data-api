75dee3e80f2336a6b58321a57d6dec73a1fd0a51
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-08-18 19:26:33 +0100

    Day 32: Add backend and fix some bugs

---
+++ backend.py
@@ -0,0 +1,18 @@
+import os
+import requests
+
+
+def get_data(place, forecast_days=None):
+    url = (f"https://api.openweathermap.org/data/2.5/forecast?"
+           f"q={place}&"
+           f"appid={os.getenv("OpenWeatherApi")}")
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    number_of_values = 8 * forecast_days
+    filtered_data = filtered_data[:number_of_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
 st.set_page_config(layout="wide")
 
@@ -13,3 +15,27 @@ days = st.slider(label="Forecast Days",
 option = st.selectbox(label="Select data to view",
                       options=("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place.capitalize()}")
+
+if place:
+    try:
+        data = get_data(place, days)
+        dates = [dict["dt_txt"] for dict in data]
+
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] - 273.15 for dict in data]
+            figure = px.line(x=dates,
+                             y=temperatures,
+                             labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in data]
+            # list comprehension or dictionary
+            st.image([f"images/{condition.lower()}.png" for condition in
+                      sky_conditions],
+                     caption=dates,
+                     width=115)
+
+    except KeyError:
+        st.error("This place is absent in the database. Please, try again.")
+
